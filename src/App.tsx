68fd48d3ad7a7777fce8c5c35b59603980d201ca
Khusniddin Khusainov <khusniddinkhusainov@ip-10-3-50-245.eu-central-1.compute.internal>
2024-11-26 16:11:37 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -9,18 +9,21 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://vite.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => {
+            setCount((count) => count + 1)
+        }}>
           count is {count}
         </button>
+        <button onClick={() => {throw new Error("This is your first error!");}}>Break the world</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,27 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+    dsn: "https://612a49d43cd554dd9b33d2075507a685@o4508364988874752.ingest.de.sentry.io/4508364992610384",
+    integrations: [
+        Sentry.browserTracingIntegration(),
+        Sentry.replayIntegration(),
+    ],
+    // Tracing
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+    tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
